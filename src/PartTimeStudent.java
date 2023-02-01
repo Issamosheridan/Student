11ebebff97190b0ae7a219a0e9b46d53e2283c59
Mohammed Issa <issamo@sheridancollege.ca>
2023-02-01 17:13:50 -0500

    PART TIME CLASS ADDED

---
+++ src/PartTimeStudent.java
@@ -0,0 +1,23 @@
+
+import studentweek2.StudentWeek2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author moeissa
+ */
+public class PartTimeStudent extends StudentWeek2 {
+    private int noOfcourses;
+
+    public int getNoOfcourses() {
+        return noOfcourses;
+    }
+
+    public void setNoOfcourses(int noOfcourses) {
+        this.noOfcourses = noOfcourses;
+    }
+}
---
+++ src/studentweek2/PartTimeStudent.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentweek2;
+
+/**
+ *
+ * @author moeissa
+ */
+public class PartTimeStudent extends StudentWeek2 {
+    private int noOfCourses;
+    public PartTimeStudent(String studentid, String studentname) {
+        super(studentid, studentname);
+        this.noOfCourses=noOfCourses;
+    }
+    
+}
